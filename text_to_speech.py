df3d00a5dedb77d5d58d114d3c1699dad88ad19b
Jaydeep Patel <64266789+jay5deep@users.noreply.github.com>
2020-09-30 20:58:46 +0530

    Add files via upload

--- text_to_speech.py
+++ text_to_speech.py
@@ -25,6 +25,12 @@ def tell_my_pos():
 
     mytext = 'Current Net Profit/Loss is: '+ str(net)
 
+	#symbol="NIFTY BANK"
+	#exchange="NFO"
+	#ltp = kite.ltp(exchange+":"+symbol)
+    #ltp = ltp[exchange+":"+symbol]['last_price']
+	#mytext = 'LTP is: '+ str(ltp)
+
     # Language in which you want to convert
     language = 'en'
 
